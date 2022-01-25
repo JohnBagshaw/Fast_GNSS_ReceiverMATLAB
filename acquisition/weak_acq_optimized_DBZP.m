7a8eac4a9ced412fb610afd1f57598fe7463e8a7
JohnBagshaw <84130776+JohnBagshaw@users.noreply.github.com>
2022-01-25 18:05:50 -0500

    Update weak_acq_optimized_DBZP.m

--- acquisition/weak_acq_optimized_DBZP.m
+++ acquisition/weak_acq_optimized_DBZP.m
@@ -1,3 +1,11 @@
+%%
+ % Project Title: GNSS-R SDR
+ % Author       : John Bagshaw
+ % Contact      : jotshaw@yorku.ca
+ % Supervisor   : Prof.Sunil Bisnath
+ % Institution  : York University, Canada.
+%%
+
 function acqResults = weak_acq_optimized_DBZP(sdrParams, ppData, rxFrameData)
 %%% This function runs acquisition algorithm known as 
 % 'Double block zero padding weak acquisition'. The minimum processing
