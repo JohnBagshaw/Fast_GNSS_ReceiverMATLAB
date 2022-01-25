29e90ffd4df2bf9284e7d49dae3d0422ceca5ec1
JohnBagshaw <84130776+JohnBagshaw@users.noreply.github.com>
2022-01-25 14:47:23 -0500

    Update post_processing.m

--- post_processing/post_processing.m
+++ post_processing/post_processing.m
@@ -1,3 +1,11 @@
+%%
+ % Project Title: GNSS-R SDR
+ % Author       : John Bagshaw
+ % Contact      : jotshaw@yorku.ca
+ % Supervisor   : Prof.Sunil Bisnath
+ % Institution  : York University, Canada.
+%%
+
 % Script postProcessing.m processes the raw signal from the specified data
 % file (in settings) operating on blocks of 37 seconds of data.
 %
@@ -7,29 +15,6 @@
 % data block, then postNavigation is called. It calculates pseudoranges
 % and attempts a position solutions. At the end plots are made for that
 % block of data.
-
-%--------------------------------------------------------------------------
-%                           SoftGNSS v3.0
-% 
-% Copyright (C) Darius Plausinaitis
-% Written by Darius Plausinaitis, Dennis M. Akos
-% Some ideas by Dennis M. Akos
-%--------------------------------------------------------------------------
-%This program is free software; you can redistribute it and/or
-%modify it under the terms of the GNU General Public License
-%as published by the Free Software Foundation; either version 2
-%of the License, or (at your option) any later version.
-%
-%This program is distributed in the hope that it will be useful,
-%but WITHOUT ANY WARRANTY; without even the implied warranty of
-%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%GNU General Public License for more details.
-%
-%You should have received a copy of the GNU General Public License
-%along with this program; if not, write to the Free Software
-%Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-%USA.
-%--------------------------------------------------------------------------
 
 %                         THE SCRIPT "RECIPE"
 %
