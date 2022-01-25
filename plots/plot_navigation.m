d4207390b346baf7360474bb672547d1a9343d02
JohnBagshaw <84130776+JohnBagshaw@users.noreply.github.com>
2022-01-25 14:40:46 -0500

    Update plot_navigation.m

--- plots/plot_navigation.m
+++ plots/plot_navigation.m
@@ -1,3 +1,11 @@
+%%
+ % Project Title: GNSS-R SDR
+ % Author       : John Bagshaw
+ % Contact      : jotshaw@yorku.ca
+ % Supervisor   : Prof.Sunil Bisnath
+ % Institution  : York University, Canada.
+%%
+
 function plot_navigation(navSolutions, settings)
 %Functions plots variations of coordinates over time and a 3D position
 %plot. It plots receiver coordinates in UTM system or coordinate offsets if
@@ -12,30 +20,7 @@ function plot_navigation(navSolutions, settings)
 %       settings        - Receiver settings. The true receiver coordinates
 %                       are contained in this structure.
 
-%--------------------------------------------------------------------------
-%                           SoftGNSS v3.0
-% 
-% Copyright (C) Darius Plausinaitis
-% Written by Darius Plausinaitis
-%--------------------------------------------------------------------------
-%This program is free software; you can redistribute it and/or
-%modify it under the terms of the GNU General Public License
-%as published by the Free Software Foundation; either version 2
-%of the License, or (at your option) any later version.
-%
-%This program is distributed in the hope that it will be useful,
-%but WITHOUT ANY WARRANTY; without even the implied warranty of
-%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%GNU General Public License for more details.
-%
-%You should have received a copy of the GNU General Public License
-%along with this program; if not, write to the Free Software
-%Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-%USA.
-%--------------------------------------------------------------------------
 
-% CVS record:
-% $Id: plotNavigation.m,v 1.1.2.25 2006/08/09 17:20:11 dpl Exp $
 
 %% Plot results in the necessary data exists ==============================
 if (~isempty(navSolutions))
