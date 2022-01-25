5ee69892b71dac02cd9e7098d18488c420e65747
JohnBagshaw <84130776+JohnBagshaw@users.noreply.github.com>
2022-01-25 14:39:15 -0500

    Update plot_acquisition_results.m

--- plots/plot_acquisition_results.m
+++ plots/plot_acquisition_results.m
@@ -1,3 +1,11 @@
+%%
+ % Project Title: GNSS-R SDR
+ % Author       : John Bagshaw
+ % Contact      : jotshaw@yorku.ca
+ % Supervisor   : Prof.Sunil Bisnath
+ % Institution  : York University, Canada.
+%%
+
 function plot_acquisition_results(sdrParams, postProcessResults)
 % SUMMAR for plotting different plots for comparison
 % of performance, code profiling and accuracy of results.
