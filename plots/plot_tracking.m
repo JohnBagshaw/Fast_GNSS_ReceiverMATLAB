0caafd7f9338e2eca44261d3b607a845e8f1f3ad
JohnBagshaw <84130776+JohnBagshaw@users.noreply.github.com>
2022-01-25 14:41:34 -0500

    Update plot_tracking.m

--- plots/plot_tracking.m
+++ plots/plot_tracking.m
@@ -1,3 +1,11 @@
+%%
+ % Project Title: GNSS-R SDR
+ % Author       : John Bagshaw
+ % Contact      : jotshaw@yorku.ca
+ % Supervisor   : Prof.Sunil Bisnath
+ % Institution  : York University, Canada.
+%%
+
 function plot_tracking(channelList, trackResults, settings)
 %This function plots the tracking results for the given channel list.
 %
@@ -8,30 +16,6 @@ function plot_tracking(channelList, trackResults, settings)
 %       trackResults    - tracking results from the tracking function.
 %       settings        - receiver settings.
 
-%--------------------------------------------------------------------------
-%                           SoftGNSS v3.0
-% 
-% Copyright (C) Darius Plausinaitis
-% Written by Darius Plausinaitis
-%--------------------------------------------------------------------------
-%This program is free software; you can redistribute it and/or
-%modify it under the terms of the GNU General Public License
-%as published by the Free Software Foundation; either version 2
-%of the License, or (at your option) any later version.
-%
-%This program is distributed in the hope that it will be useful,
-%but WITHOUT ANY WARRANTY; without even the implied warranty of
-%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%GNU General Public License for more details.
-%
-%You should have received a copy of the GNU General Public License
-%along with this program; if not, write to the Free Software
-%Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-%USA.
-%--------------------------------------------------------------------------
-
-%CVS record:
-%$Id: plotTracking.m,v 1.5.2.23 2006/08/14 14:45:14 dpl Exp $
 
 % Protection - if the list contains incorrect channel numbers
 channelList = intersect(channelList, 1:settings.numberOfChannels);
