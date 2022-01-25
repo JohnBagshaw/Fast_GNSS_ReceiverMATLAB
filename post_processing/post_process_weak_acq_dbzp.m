5bc04edd13f5ca314be609e834693680a5f552cb
JohnBagshaw <84130776+JohnBagshaw@users.noreply.github.com>
2022-01-25 14:45:56 -0500

    Update post_process_weak_acq_dbzp.m

--- post_processing/post_process_weak_acq_dbzp.m
+++ post_processing/post_process_weak_acq_dbzp.m
@@ -1,3 +1,11 @@
+%%
+ % Project Title: GNSS-R SDR
+ % Author       : John Bagshaw
+ % Contact      : jotshaw@yorku.ca
+ % Supervisor   : Prof.Sunil Bisnath
+ % Institution  : York University, Canada.
+%%
+
 function acqResults = post_process_weak_acq_dbzp(...
     sdrParams, ...
     dataFileParams, ...
